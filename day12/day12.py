6ae96241a054f5b6f98372112c63dafaaf0fcb26
readjfb <readjfb@aol.com>
2021-12-12 02:00:46 -0600

    Day 12 complete using recursive backtracking.
    I feel like there's a more efficient way to do this, but this is the obvious solution.

---
+++ day12/day12.py
@@ -0,0 +1,44 @@
+class Cave:
+    def __init__(self, id, large):
+        self.id = id
+        self.children = []
+        self.large = large
+
+
+with open("day12input.txt") as f:
+    lines = f.readlines()
+
+cut_lines = [line.strip().split("-") for line in lines]
+combined = [set(item) for item in cut_lines]
+combined = set().union(*combined)
+
+cave_ids = {}
+for cave in combined:
+    cave_ids[cave] = Cave(cave, cave.upper() == cave)
+
+for line in cut_lines:
+    cave_ids[line[0]].children.append(cave_ids[line[1]])
+    cave_ids[line[1]].children.append(cave_ids[line[0]])
+
+
+def turtle(cave, visited, twice):
+    return_list = []
+    visited.append(cave.id)
+
+    if cave == cave_ids["end"]:
+        return [visited] + return_list
+
+    for child in cave.children:
+        if child.large or child.id not in visited:
+            return_list += turtle(child, visited[:], twice)
+        elif not twice and child.id in visited and child != cave_ids["start"]:
+            return_list += turtle(child, visited[:], True)
+
+    return return_list
+
+
+paths1 = turtle(cave_ids["start"], [], True)
+print("Part 1:", len(paths1))
+
+paths2 = turtle(cave_ids["start"], [], False)
+print("Part 2:", len(paths2))

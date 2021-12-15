a15ef97ef45c9862c17e0102f03db1b4d20c59be
readjfb <15671813+readjfb@users.noreply.github.com>
2021-12-15 17:08:36 -0600

    Minor changes to day 15; biggest bottleneck is that I implemented my own heap for practice, which is inherently slow (Python is slow)

--- day15/day15.py
+++ day15/day15.py
@@ -1,17 +1,17 @@
 with open("day15input.txt") as f:
     data = f.read().strip()
 
-distances = [[int(x) for x in line] for line in data.split("\n")]
+risks = [[int(x) for x in line] for line in data.split("\n")]
 
 
 class MyHeap:
     #  Heap where pop returns the element with the smallest value
     def __init__(self) -> None:
         self.heap = []
-        self.heap_set = set()
 
     def push(self, value, item) -> None:
-        upper_bound, lower_bound = len(self.heap) - 1, 0
+        upper_bound =  len(self.heap) - 1
+        lower_bound = 0
 
         # Use binary search to find the correct position for insertion
         while lower_bound <= upper_bound:
@@ -21,56 +21,50 @@ class MyHeap:
             else:
                 upper_bound = mid - 1
 
-        if item not in self.heap_set:
-            self.heap.insert(lower_bound, (value, item))
-
-            self.heap_set.add(item)
+        self.heap.insert(lower_bound, (value, item))
 
     def pop(self) -> tuple:
-        v = self.heap.pop()
-        self.heap_set.remove(v[1])
-
-        return v
+        return self.heap.pop()
 
 
-def find_shortest_path(distances) -> int:
+def find_shortest_path(risks) -> int:
     finalized = set()
     heap = MyHeap()
-    heap.push(0, (len(distances[0]) - 1, len(distances) - 1))
+    heap.push(0, (len(risks[0]) - 1, len(risks) - 1))
 
     while heap.heap:
         value, item = heap.pop()
 
         if item == (0, 0):
-            return value - distances[0][0] + distances[-1][-1]
+            return value - risks[0][0] + risks[-1][-1]
+
+        if item in finalized:
+            continue
 
         finalized.add(item)
 
         x, y = item
 
-        for neighbor in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
-            nx, ny = neighbor
-            nx = x + nx
-            ny = y + ny
+        for neigh in [(x, y + 1), (x + 1, y), (x, y - 1), (x - 1, y)]:
             if (
-                len(distances) > ny >= 0
-                and len(distances[0]) > nx >= 0
-                and (nx, ny) not in finalized
+                len(risks) > neigh[1] >= 0
+                and len(risks[0]) > neigh[0] >= 0
+                and neigh not in finalized
             ):
-                heap.push(distances[ny][nx] + value, (nx, ny))
+                heap.push(risks[neigh[1]][neigh[0]] + value, neigh)
 
     print("ERROR NO PATH FOUND!")
     return None
 
 
-print("Part 1:", find_shortest_path(distances))
+print("Part 1:", find_shortest_path(risks))
 
-full_distances = [[0] * len(distances[0]) * 5 for _ in range(len(distances) * 5)]
+full_distances = [[0] * len(risks[0]) * 5 for _ in range(len(risks) * 5)]
 
-for i in range(0, 5 * len(distances[0]), len(distances[0])):
-    for j in range(0, 5 * len(distances), len(distances)):
-        for k in range(len(distances[0])):
-            for l in range(len(distances)):
-                full_distances[i + k][j + l] = ((distances[k][l] - 1 + i + j) % 9) + 1
+for i in range(0, 5 * len(risks[0]), len(risks[0])):
+    for j in range(0, 5 * len(risks), len(risks)):
+        for k in range(len(risks[0])):
+            for l in range(len(risks)):
+                full_distances[i + k][j + l] = ((risks[k][l] - 1 + i + j) % 9) + 1
 
 print("Part 2:", find_shortest_path(full_distances))
